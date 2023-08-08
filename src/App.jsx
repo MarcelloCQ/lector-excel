fb810440602f259f428bba63c701525127646154
MarcelloCQ <giosuemc@gmail.com>
2023-08-08 00:45:41 -0500

    router change

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import './App.css'
 import { Dashboard } from './pages/Dashboard/Dashboard'
 import { Organigrama } from './pages/Organigrama/Organigrama'
-import { Provider } from 'react-redux'
-import { store } from './redux/app/store'
-import { Route, Routes } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 
-function App() {
+const routes = createHashRouter([
+  {
+    path: "/",
+    element: <Organigrama />
+  },
+  {
+    path: "/organigrama",
+    element: <Organigrama />
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />
+  }
+])
 
-  return (
-    <>
-      <Provider store={store}>
-        <Routes>
-          <Route path='/' element={<Organigrama/>} />
-          <Route path='/organigrama' element={<Organigrama />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-        </Routes>
-      </Provider>
-    </>
-  )
-}
-
-export default App
+export default routes;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import router from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './redux/app/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store} >
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
