393ad155f2a8a8c173c096445513f8b27627f8c9
MarcelloCQ <giosuemc@gmail.com>
2023-08-07 19:04:17 -0500

    name change

--- src/components/FotoPerfil/FotoPerfil.jsx
+++ src/components/FotoPerfil/FotoPerfil.jsx
@@ -1,4 +1,4 @@
-import fotoPerfil from './../../assets/icono-gris-de-perfil-usuario-símbolo-empleado-avatar-web-y-diseño-ilustración-signo-aislado-en-fondo-blanco-191067342.jpg'
+import fotoPerfil from './../../assets/icono-gris.jpg'
 
 export const FotoPerfil = () => {
   return (
